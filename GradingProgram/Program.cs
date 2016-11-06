45647e446084c5f0976fdfea8817684551a3e0cb
robinlyang <robiny@gmail.com>
2016-11-05 22:08:17 -0600

    Initial Project

--- GradingProgram/Program.cs
+++ GradingProgram/Program.cs
@@ -6,10 +6,19 @@ using System.Threading.Tasks;
 
 namespace GradingProgram
 {
+    class Grading
+    {
+        public double
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Grading g = Grading(7, 6, 8, 85, 95); //q1, q2, q3, midterm, final
+            Console.WriteLine(g.overall_grade);
+            Console.WriteLine(g.letter);
+            Console.WriteLine(g.midterm);
+            Console.ReadLine();
         }
     }
 }
